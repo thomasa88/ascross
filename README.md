8c3f900ce1a2ba947e8fa2d77c7c1447b2060bd4
Thomas Axelsson <thomasa88@gmail.com>
2023-12-10 20:46:19 +0100

    Page number in samples. Screenshots.

---
+++ README.md
@@ -0,0 +1,2 @@
+![](screenshot_a4.png)
+![](screenshot_a5two.png)
--- generate_samples.sh
+++ generate_samples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 ascross.py crosswords.samples/*.toml --format a4 --output crosswords.samples/a4.html
-python3 ascross.py crosswords.samples/*.toml --format a5two --output crosswords.samples/a5two.html
-python3 ascross.py crosswords.samples/*.toml --format a5two --solution --output crosswords.samples/a5two_solutions.html
+python3 ascross.py crosswords.samples/*.toml --format a4 --page-num 1 --output crosswords.samples/a4.html
+python3 ascross.py crosswords.samples/*.toml --format a5two --page-num 2 --output crosswords.samples/a5two.html
+python3 ascross.py crosswords.samples/*.toml --format a5two --page-num 2 --solution --output crosswords.samples/a5two_solutions.html
 python3 ascross.py crosswords.samples/thomas_minikrypto.toml --format svg --output crosswords.samples/thomas_minikrypto.svg
